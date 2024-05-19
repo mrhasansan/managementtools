23392cbead450eef7a67309bcac64d99f44ee795
Hasansan <hasansan@Hasansans-MacBook.local>
2024-05-19 12:56:20 +0700

    add todo items

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,6 +1,6 @@
 import { dataTodos } from "../data/todos";
-import { useState } from "react";
 import { TodoItem } from "./todo-item";
+import { useState } from "react";
 
 export default function Body() {
   const name = "Maruf Hasan";
@@ -11,6 +11,7 @@ export default function Body() {
       <div>
         <input required type="text" value={addToDo} className=" border border-gray-50 rounded-md px-4 m-4" placeholder="Add a new to-do" onChange={(e) => setAddToDo(e.target.value)} />
         <button className=" bg-sky-500 rounded-md mx-4 px-4">Add</button>
+
         <ul>
           {dataTodos.map((todo) => (
             <li key={todo.id}>
--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -3,14 +3,13 @@ import { type Todo } from "../data/todos";
 
 export function TodoItem({ todo }: { todo: Todo }) {
   const [showMore, setShowMore] = useState<boolean>(false);
-
   function handleClick() {
     setShowMore(!showMore);
   }
-
   return (
     <div>
       <li>{todo.title}</li>
+      <p>{todo.status}</p>
       <button className=" bg-slate-500 rounded-lg w-auto px-4" onClick={handleClick}>
         {showMore ? "Hide Details" : "Show Details"}
       </button>
