34eecf2db2c15396083cbc4129fadb44f360b67a
mrhasansan <marufhasansan@gmail.com>
2024-06-02 13:16:21 +0700

    repair sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+export function Sidebar() {
+  return (
+    <div className="w-1/4 bg-gray-200 p-4">
+      <nav className="space-y-2">
+        <a href="#" className="block p-2 rounded hover:bg-blue-500 hover:text-white">
+          My Day
+        </a>
+        <a href="#" className="block p-2 rounded hover:bg-blue-500 hover:text-white">
+          Important
+        </a>
+        <a href="#" className="block p-2 rounded hover:bg-blue-500 hover:text-white">
+          Planned
+        </a>
+        <a href="#" className="block p-2 rounded hover:bg-blue-500 hover:text-white">
+          Assigned to me
+        </a>
+        <a href="#" className="block p-2 rounded hover:bg-blue-500 hover:text-white">
+          Tasks
+        </a>
+        <button className="block p-2 rounded bg-blue-500 text-white">New List</button>
+      </nav>
+    </div>
+  );
+}
